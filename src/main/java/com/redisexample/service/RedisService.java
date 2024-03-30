694bc7b51b39aa2bd9463e402604029f084f950b
Krishna <gkrrish.11@gmail.com>
2024-03-30 13:05:23 +0530

    added the Redis keys

--- src/main/java/com/redisexample/service/RedisService.java
+++ src/main/java/com/redisexample/service/RedisService.java
@@ -1,22 +1,29 @@
 package com.redisexample.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisService {
+	
+	//Used Redis Insight for UI REDIS
 
 	@Autowired
 	private StringRedisTemplate redisTemplate;
 
 	public void setHelloWorldKey() {
-		// Setting the value of the key "hello" to "Hello, World!"
-		redisTemplate.opsForValue().set("hello", "Hello, World!");
+		String KEY = "Hello";
+		String VALUE = "Hello-World! Redis..";
+		long timeout = 2;
+		TimeUnit timeUnit = TimeUnit.MINUTES;
+
+		redisTemplate.opsForValue().set(KEY, VALUE, timeout, timeUnit);
 	}
 
 	public String getHelloWorldValue() {
-		// Retrieving the value of the key "hello"
 		return redisTemplate.opsForValue().get("hello");
 	}
 
